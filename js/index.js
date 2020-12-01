6a619274861e6381496b51e686fa199066e7453d
たた <pf35301@gmail.com>
2020-12-01 16:14:37 +0900

    [fix] path

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ window.onload = function() {
         ["あらぶるペンギン", "FireEmblem"],
         ["Raito", "Persona"],
         ["ゅゅゅ", "DK"],
-        ["かなん", "EarthBound"],
+        ["かなん", "Earthbound"],
         ["りんごまん", "MetalGear"],
         ["たた", "StreetFighter", "pf35301"],
         ["ウォッチA96992", "Pokemon"],
